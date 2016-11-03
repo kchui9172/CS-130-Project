901968aa2a53f4da3a31d1f8c9d80956d52c3169
Zachary Yee <zachyee@ucla.edu>
2016-11-02 21:59:14 -0700

    Fix spacing and indentation

--- js/components/MessageComponent.js
+++ js/components/MessageComponent.js
@@ -4,45 +4,46 @@ import TodoList from './TodoList.js';
 import ToDoItem from './ToDoItem.js';
 
 export default class Messages extends React.Component{
-	constructor() {
-		super();
-   };
+    constructor() {
+        super();
+    };
 
-   render(){
-   		return (
+    render(){
+        return (
             <TodoList/>
-   // 			<div classname="messageClass">
-   // 				<div className="messageTitle">
-   // 					<h1>Messages</h1>
-   // 				</div>
-            //
-   // 				<Row className="show-grid">
-   // 					<Col sm={4} className="mCol">
-   // 						<div className="inner-col">
-   // 							<h2>Groceries</h2>
-   // 							<div class="list1">
-   // 								<TodoList l1/>
-   // 							</div>
-   // 						</div>
-   // 					</Col>
-   // 					<Col sm={4} className="mCol">
-   // 					 	<div className="inner-col">
-   // 							<h2>Meetings</h2>
-   // 							<div class="list2">
-   // 								<TodoList l2/>
-   // 							</div>
-   // 						</div>
-   // 					</Col>
-   // 					<Col sm={4} className="mCol">
-   // 					   	<div className="inner-col">
-   // 							<h2>Misc</h2>
-   // 							<div class="list3">
-   // 								<TodoList l3/>
-   // 							</div>
-   // 						</div>
-   // 					</Col>
-   // 				</Row>
-   // 			</div>
-		);
-   }
+/*
+            <div classname="messageClass">
+                <div className="messageTitle">
+                    <h1>Messages</h1>
+                </div>
+                <Row className="show-grid">
+                    <Col sm={4} className="mCol">
+                        <div className="inner-col">
+                            <h2>Groceries</h2>
+                            <div class="list1">
+                                <TodoList l1/>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col sm={4} className="mCol">
+                        <div className="inner-col">
+                            <h2>Meetings</h2>
+                            <div class="list2">
+                                <TodoList l2/>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col sm={4} className="mCol">
+                        <div className="inner-col">
+                            <h2>Misc</h2>
+                            <div class="list3">
+                                <TodoList l3/>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+*/
+	);
+    }
 }
